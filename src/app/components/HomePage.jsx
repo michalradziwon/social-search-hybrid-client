f2b753ae553baeec38c5c6a7ea6eeff2132ed117
Michal <michal.radzi.won@gmail.com>
2015-02-02 23:30:02 +0000

    basic navigation with tabs

--- src/app/components/HomePage.jsx
+++ src/app/components/HomePage.jsx
@@ -1,10 +1,36 @@
 var React = require('react');
+var mui = require('material-ui');
+var {
+  Tab,Tabs
+  } = mui;
 
 var Home = React.createClass({
+  componentDidMount: function(){
+  },
   render: function () {
     return (
-      <div className="centralized">
-        Home page.
+      <div>
+        <Tabs>
+          <Tab label="Tab one" >
+            <div className="tab-template-container">
+              <h2 className="mui-font-style-headline">Welcome in Dendryt!</h2>
+              <p>
+              How do you feel today&#63;
+              </p>
+              <p>
+              Can I help you&#63;
+              </p>
+            </div>
+          </Tab>
+          <Tab label="Item Two" >
+            <div className="tab-template-container">
+              <h2 className="mui-font-style-headline">Another page</h2>
+              <p>
+              Hi Again.
+              </p>
+            </div>
+          </Tab>
+        </Tabs>
       </div>
     );
   }
